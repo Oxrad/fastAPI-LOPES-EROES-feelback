5ea756f19d98e465c0f234714706e9bd7579fba7
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2025-10-23 10:33:23 +0200

    feat: Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,21 @@ import './style/App.css'
 import Home from "./pages/Home.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import FeelbackForm from "./pages/FeelbackForm.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
 
   return (
     <>
       <Header />
-      <main>
+      <>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/feelback-form" element={<FeelbackForm />} />
         </Routes>
-      </main>
+      </>
+      <Footer />
     </>
   )
 }
