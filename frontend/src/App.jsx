8055fbdc4b8057ddca46e6d6b3428d3f27d2d209
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2025-10-23 10:12:59 +0200

    feat: routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-
+import { Routes, Route } from "react-router-dom";
 import Header from './components/Header'
 import './style/App.css'
+
+import Home from "./pages/Home.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import FeelbackForm from "./pages/FeelbackForm.jsx";
 
 function App() {
 
   return (
     <>
       <Header />
-      {/* <main className="p-6">
+      <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/form" element={<FormPage />} />
+          <Route path="/feelback-form" element={<FeelbackForm />} />
         </Routes>
-      </main> */}
+      </main>
     </>
   )
 }
---
+++ frontend/src/pages/FeelbackForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FeelbackForm() {
+  return (
+    <div>FeelbackForm</div>
+  )
+}
+
+export default FeelbackForm
