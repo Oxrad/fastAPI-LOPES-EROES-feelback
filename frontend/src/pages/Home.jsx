cb25a55fb5db76f163c99e0d7a37696940dfb8f9
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2025-10-23 10:25:39 +0200

    feat: home structure

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import '../style/Home.scss'
 
 function Home() {
   return (
-    <div>Home</div>
+    <main>
+        <button className='secondary'>CONSULTER LE TABLEAU DE BORD</button>
+        <h1>Bienvenue sur l'application FeelBack</h1>
+        <p>Cette application vous permet d'évaluer la livraison de votre commande</p>
+        <button className='primary'>CRÉER UNE COMMANDE FICTIVE ET RÉPONDRE AU QUESTIONNAIRE DE SATISFACTION</button>
+    </main>
   )
 }
 
